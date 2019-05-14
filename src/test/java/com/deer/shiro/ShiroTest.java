de68e9e19df2ce48e474ad6a4ac7b3073b81066e
Mr_Deer <mr_deer_y@163.com>
2019-05-14 13:32:46 +0800

    笔记更新 - 授权完成

--- src/test/java/com/deer/shiro/ShiroTest.java
+++ src/test/java/com/deer/shiro/ShiroTest.java
@@ -57,7 +57,7 @@ public class ShiroTest {
     }
 
     /**
-     * Shiro 使用 ini 文件做授权测试
+     * Shiro 使用 ini 文件做授权测试（角色）
      * <p>
      * 注意：做授权测试的前提一定是认证通过，否则不能进行授权测试
      */
@@ -82,7 +82,7 @@ public class ShiroTest {
         // 8. 判断用户是否拥有单个角色(true: 拥有该角色/ false：没有该角色)
 //        boolean flag = subject.hasRole("role1");
         // 8. 判断用户是否拥有全部角色(true：拥有全部角色/ false：不满足全部角色)
-        List<String> list = Arrays.asList("role1", "role2","role3");
+        List<String> list = Arrays.asList("role1", "role2", "role3");
 //        boolean flag = subject.hasAllRoles(list);
         // 8. 判断用户匹配哪个角色，返回一个 boolean 集合，匹配成功的返回 true，失败的返回 false
         boolean[] booleans = subject.hasRoles(list);
@@ -91,5 +91,46 @@ public class ShiroTest {
 
         // 8. 通过 checkRoles() 方法来进行匹配。匹配成功不做任何操作，有一个匹配失败则直接抛出 UnauthorizedException 异常
         subject.checkRoles(list);
+    }
+
+    /**
+     * Shiro 使用 ini 文件做授权测试（权限）
+     * <p>
+     * 注意：做授权测试的前提一定是认证通过，否则不能进行授权测试
+     */
+    @Test
+    public void permitTest() {
+        // 1. 获取默认的管理管理器 SecurityManager
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+        // 2. 创建一个 Realm，同时把我们写好的 ini 文件路径放进来
+        IniRealm iniRealm = new IniRealm("classpath:shiro/shiro.ini");
+        // 3. 设置 Realm
+        defaultSecurityManager.setRealm(iniRealm);
+        // 4. 将 SecurityManager 绑定到当前环境中，注意这里的 SecurityUtils 引用的是：org.apache.shiro.SecurityUtils 不要引用错误了
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+        // 5. 创建当前登陆的主体
+        Subject subject = SecurityUtils.getSubject();
+        // 6. 创建主体登录所需要的认证信息对象 token。也就是我们的用户名和密码
+        String username = "userA";
+        String password = "111";
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        // 7. 登录认证
+        subject.login(token);
+        // 8. 判断用户是否拥有单个权限(true: 拥有该权限/ false：没有该权限)
+//        boolean flag = subject.isPermitted("user:create");
+//        boolean flag = subject.isPermitted("department:create");
+//        System.out.println("是否拥有该权限 = = = = = = = = = "+flag);
+
+        // 8. 判断用户是否拥有全部权限(true: 拥有全部权限/ false：其中有一个不满足)
+//        boolean flag = subject.isPermittedAll("user:create", "user:update");
+//        boolean flag = subject.isPermittedAll("user:create", "user:update","department:create");
+//        System.out.println("是否拥有全部权限 = = = = = = = = = "+flag);
+
+        // 8. 判断用户是否拥有部分权限(true: 拥有该权限/ false：没有该群贤)
+        boolean[] flag = subject.isPermitted("user:create", "user:update", "department:create");
+        System.out.println("部分结果 = = = = = = = = = " + Arrays.toString(flag));
+
+        // 8. 判断用户是否拥有单个权限，拥有则没有任何操作，不拥有则抛出 UnauthorizedException 异常
+        subject.checkRole("department:create");
     }
 }
